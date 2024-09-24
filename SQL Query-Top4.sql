75cb16f31f7025eebd946d35fe33afe8ff66ccd2
YvonneLipLim <164453695+YvonneLipLim@users.noreply.github.com>
2024-09-24 20:25:34 +0800

    Update SQL Query-Top4.sql

--- SQL Query-Top4.sql
+++ SQL Query-Top4.sql
@@ -3,70 +3,70 @@
 CREATE VIEW Top4_Winning_Teams AS
 WITH CombinedData AS (
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '2022' AS Year
-    FROM FIFA.dbo.FIFA2022
+    FROM FIFA.dbo.Year2022
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '2018' AS Year
-    FROM FIFA.dbo.FIFA2018
+    FROM FIFA.dbo.Year2018
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '2014' AS Year
-    FROM FIFA.dbo.FIFA2014
+    FROM FIFA.dbo.Year2014
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '2010' AS Year
-    FROM FIFA.dbo.FIFA2010
+    FROM FIFA.dbo.Year2010
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '2006' AS Year
-    FROM FIFA.dbo.FIFA2006
+    FROM FIFA.dbo.Year2006
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '2002' AS Year
-    FROM FIFA.dbo.FIFA2002
+    FROM FIFA.dbo.Year2002
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1998' AS Year
-    FROM FIFA.dbo.FIFA1998
+    FROM FIFA.dbo.Year1998
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1994' AS Year
-    FROM FIFA.dbo.FIFA1994
+    FROM FIFA.dbo.Year1994
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1990' AS Year
-    FROM FIFA.dbo.FIFA1990
+    FROM FIFA.dbo.Year1990
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1986' AS Year
-    FROM FIFA.dbo.FIFA1986
+    FROM FIFA.dbo.Year1986
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1982' AS Year
-    FROM FIFA.dbo.FIFA1982
+    FROM FIFA.dbo.Year1982
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1978' AS Year
-    FROM FIFA.dbo.FIFA1978
+    FROM FIFA.dbo.Year1978
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1974' AS Year
-    FROM FIFA.dbo.FIFA1974
+    FROM FIFA.dbo.Year1974
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1970' AS Year
-    FROM FIFA.dbo.FIFA1970
+    FROM FIFA.dbo.Year1970
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1966' AS Year
-    FROM FIFA.dbo.FIFA1966
+    FROM FIFA.dbo.Year1966
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1962' AS Year
-    FROM FIFA.dbo.FIFA1962
+    FROM FIFA.dbo.Year1962
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1958' AS Year
-    FROM FIFA.dbo.FIFA1958
+    FROM FIFA.dbo.Year1958
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1954' AS Year
-    FROM FIFA.dbo.FIFA1954
+    FROM FIFA.dbo.Year1954
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1950' AS Year
-    FROM FIFA.dbo.FIFA1950
+    FROM FIFA.dbo.Year1950
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1938' AS Year
-    FROM FIFA.dbo.FIFA1938
+    FROM FIFA.dbo.Year1938
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1934' AS Year
-    FROM FIFA.dbo.FIFA1934
+    FROM FIFA.dbo.Year1934
     UNION ALL
     SELECT [Position], [Team], [Win], [Draw], [Loss], [Goals_For] AS GoalsScored, [Goals_Against] AS GoalsLoss, [Points], '1930' AS Year
-    FROM FIFA.dbo.FIFA1930
+    FROM FIFA.dbo.Year1930
 )
 SELECT Year, Team, Win, Draw, Loss, GoalsScored, GoalsLoss, Points, TeamRank
 FROM (
@@ -82,6 +82,6 @@ SELECT [Year],
     MAX(CASE WHEN TeamRank = 2 THEN Team END) AS "Runners_Up",
     MAX(CASE WHEN TeamRank = 3 THEN Team END) AS "Third_Place",
     MAX(CASE WHEN TeamRank = 4 THEN Team END) AS "Forth_Place"
-FROM dbo.Top4_WinningTeams
+FROM dbo.Top4_Winning_Teams
 GROUP BY Year
 ORDER BY [Year] DESC;
