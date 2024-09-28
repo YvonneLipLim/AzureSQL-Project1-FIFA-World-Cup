1e459f7bf4b7244f37b0be9452453bed6fd93764
YvonneLipLim <164453695+YvonneLipLim@users.noreply.github.com>
2024-09-28 22:02:50 +0800

    Update Query_Results.sql

--- Query_Results.sql
+++ Query_Results.sql
@@ -20,7 +20,7 @@ ORDER BY [Year] DESC;
 
 
 --Q3. Which were the teams that were eliminated at the quarter-final stage?
---The results would display teams who didn't enter quarter-final stage.
+--The results would display teams who didn't enter the quarter-final stage.
 SELECT DISTINCT wc.Year, wc.TeamRank, wc.Team AS Team, tc.Continent, wc.TotalGamesPlayed, wc.Win, wc.Draw, wc.Loss, wc.GoalsScored, wc.GoalsLoss, wc.Points
 FROM FIFA.dbo.WorldCup_Combined_Results wc
 JOIN dbo.TeamContinents tc ON wc.Team = tc.TeamName
@@ -28,8 +28,8 @@ WHERE [TeamRank] > 8
 ORDER BY [Year] DESC;
 
 
---Q4. Which teams had won the most champion titles from 1930 to 2022?
---The results would display top teams total wins for all World Cups.
+--Q4. Which teams had won the most World Cup titles from 1930 to 2022?
+--The results would display the team with the most titles and the total wins for all World Cups.
 SELECT *
 FROM (
     SELECT 
@@ -48,8 +48,15 @@ ORDER BY Champion DESC, TotalWins DESC, Team ASC;
 
 
 --Q5. Which teams scored the most goals throughout the World Cups?
---The results would display team with total goals scored for all World Cups.
-SELECT Team, SUM(GoalsScored) AS TotalGoalsScored
-FROM dbo.WorldCup_Combined_Results
+--The results would display the team with total goals scored for all World Cups.
+SELECT Team,  
+        SUM(Win) AS TotalWin,
+        SUM(Draw) AS TotalDraw,
+        SUM(Loss) AS TotalLost,
+        SUM(GoalsScored) AS TotalGoalsScored, 
+        SUM(GoalsLoss) AS TotalGoalsLoss,
+        SUM(GoalsScored) - SUM(GoalsLoss) AS GoalsDifference,
+        SUM((Win) * 3 + Draw * 1) AS TotalPoints
+FROM FIFA.dbo.WorldCup_Combined_Results
 GROUP BY Team
-ORDER BY TotalGoalsScored DESC; 
+ORDER BY TotalPoints DESC, Team DESC;
