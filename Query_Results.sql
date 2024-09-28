e8392c61a8a658c2a98d686f16706d27000a5c2c
YvonneLipLim <164453695+YvonneLipLim@users.noreply.github.com>
2024-09-28 19:05:54 +0800

    Update Query_Results.sql

--- Query_Results.sql
+++ Query_Results.sql
@@ -7,25 +7,29 @@ ORDER BY [Year] DESC;
 
 
 --Q2. Which were the teams that reached the top 4 from 1930 to 2022?
+--The results would be the Top 4 Winning Teams for each World Cup Year including a new column for Total Teams participated.
 SELECT [Year],
     MAX(CASE WHEN TeamRank = 1 THEN Team END) AS Champion,
     MAX(CASE WHEN TeamRank = 2 THEN Team END) AS Runner_Up,
     MAX(CASE WHEN TeamRank = 3 THEN Team END) AS Third_Place,
-    MAX(CASE WHEN TeamRank = 4 THEN Team END) AS Fourth_Place
+    MAX(CASE WHEN TeamRank = 4 THEN Team END) AS Fourth_Place,
+    COUNT(DISTINCT Team) AS TotalTeams
 FROM dbo.WorldCup_Combined_Results
 GROUP BY [Year]
 ORDER BY [Year] DESC;
 
 
---Q3. Which were the teams that were eliminated at the knock-out stage from 1930 to 2022?
-SELECT wc.Year, wc.TeamRank, wc.Team AS Team, tc.Continent, wc.TotalGamesPlayed, wc.Win, wc.Draw, wc.Loss, wc.GoalsScored, wc.GoalsLoss, wc.Points
+--Q3. Which were the teams that were eliminated at the quarter-final stage?
+--The results would display teams who didn't enter quarter-final stage.
+SELECT DISTINCT wc.Year, wc.TeamRank, wc.Team AS Team, tc.Continent, wc.TotalGamesPlayed, wc.Win, wc.Draw, wc.Loss, wc.GoalsScored, wc.GoalsLoss, wc.Points
 FROM FIFA.dbo.WorldCup_Combined_Results wc
 JOIN dbo.TeamContinents tc ON wc.Team = tc.TeamName
-WHERE [TeamRank] > 16
+WHERE [TeamRank] > 8
 ORDER BY [Year] DESC;
 
 
 --Q4. Which teams had won the most champion titles from 1930 to 2022?
+--The results would display top teams total wins for all World Cups.
 SELECT *
 FROM (
     SELECT 
@@ -44,6 +48,7 @@ ORDER BY Champion DESC, TotalWins DESC, Team ASC;
 
 
 --Q5. Which teams scored the most goals throughout the World Cups?
+--The results would display team with total goals scored for all World Cups.
 SELECT Team, SUM(GoalsScored) AS TotalGoalsScored
 FROM dbo.WorldCup_Combined_Results
 GROUP BY Team
